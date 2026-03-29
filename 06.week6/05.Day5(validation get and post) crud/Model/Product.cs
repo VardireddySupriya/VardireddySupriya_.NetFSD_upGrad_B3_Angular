615bf9ef49d8478443f3b49194e97474765beb2c
VardireddySupriya <rsupriya2916@gmail.com>
2026-03-29 23:10:56 +0530

    Add files via upload

---
+++ 06.week6/05.Day5(validation get and post) crud/Model/Product.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class Product
+    {
+        [Required]
+        public int ProductId {  get; set; }
+
+        [Required(ErrorMessage = "Product Name is required")]
+        [StringLength(15, MinimumLength = 5, ErrorMessage = "Name must be 5 to 15 characters")]
+        public string ProductName{ get; set; }
+
+        [Required(ErrorMessage = "Price is required")]
+        public decimal Price { get; set; }
+
+        [StringLength(15, MinimumLength = 5, ErrorMessage = "Category must be 5 to 15 characters")]
+        public string Category {  get; set; }
+        public int quantity {  get; set; }
+    }
+}
