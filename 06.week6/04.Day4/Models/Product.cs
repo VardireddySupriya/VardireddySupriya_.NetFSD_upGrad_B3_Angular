5953bb3ac016f33269f7f315add4b88fb5d7c724
VardireddySupriya <rsupriya2916@gmail.com>
2026-03-29 21:57:48 +0530

    Add files via upload

---
+++ 06.week6/04.Day4/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication1.Models
+{
+    public class Product
+    {
+        public int ProductId {  get; set; }
+        public string ProductName{ get; set; }
+        public decimal Price { get; set; }
+        public string Category {  get; set; }
+        public int quantity {  get; set; }
+    }
+}
