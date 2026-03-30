64559be3c12bc33cf61e0fd38c9dfc9dd6d03df0
VardireddySupriya <rsupriya2916@gmail.com>
2026-03-30 23:19:05 +0530

    Add files via upload

---
+++ 07.Week7/01.Day1/Models/ContactInfo.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class ContactInfo
+    {
+        [Required]
+        public int ContactId {  get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        public String CompanyName {  get; set; }
+        [EmailAddress]
+        public String EmailId { get; set; }
+
+        [RegularExpression(@"^[0-9]{10}$", ErrorMessage = "Mobile number must be 10 digits")]
+        public long MobileNo{ get; set; }
+        public string Designation {  get; set; }
+    }
+}
