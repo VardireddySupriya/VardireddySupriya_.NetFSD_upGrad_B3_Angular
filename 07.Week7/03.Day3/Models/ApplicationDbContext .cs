4ae434503527f6819d199fd38e6ac3cbfa691ac3
VardireddySupriya <rsupriya2916@gmail.com>
2026-04-06 09:54:15 +0530

    Add files via upload

---
+++ 07.Week7/03.Day3/Models/ApplicationDbContext .cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Data;
+
+namespace WebApplication2.Models
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+ :      base(options)
+        {
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        //public DbSet<Employee> Employees { get; set; }
+        //public DbSet<ContactInfo> contactInfo { get; set; }
+    }
+}
---
+++ 07.Week7/03.Day3/Models/ContactInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication2.Models
+{
+    public class ContactInfo
+    {
+        public  int ContactId {  get; set; }//primary key
+        [Required]
+        public string FirstName {  get; set; }
+        [Required]
+        public string LastName { get; set; }
+        public string CompanyName {  get; set; }
+        [EmailAddress]
+        public string EmailId {  get; set; }
+        [RegularExpression(@"^[0-9]{10}$",ErrorMessage = "Mobile number must be 10 digits")]
+        public long MobileNo {  get; set; }
+        public string Designation {  get; set; }
+    }
+}
