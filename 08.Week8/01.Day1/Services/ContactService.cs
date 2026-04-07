df7be32975a9e203d29ace1d9e70e1a1a26f1e11
VardireddySupriya <rsupriya2916@gmail.com>
2026-04-07 14:54:17 +0530

    Add files via upload

---
+++ 08.Week8/01.Day1/Services/ContactService.cs
@@ -0,0 +1,34 @@
+﻿using WebApplication7.Models;
+using WebApplication7.Repository;
+
+namespace WebApplication7.Services
+{
+    public class ContactService:IContactService
+    {
+        private readonly IContactRepository _repo;
+        public ContactService(IContactRepository repo)
+        {
+            _repo = repo;
+        }
+        public IEnumerable<ContactInfo> GetAllContacts()
+        {
+            return _repo.GetAllContacts();
+        }
+        public ContactInfo GetContactById(int id)
+        {
+            return _repo.GetContactById(id);
+        }
+        public void AddContact(ContactInfo contact)
+        {
+           _repo.AddContact(contact);
+        }
+        public void UpdateContact(ContactInfo contact)
+        {
+            _repo.UpdateContact(contact);
+        }
+        public void DeleteContact(int id)
+        {
+            _repo.DeleteContact(id);
+        }
+    }
+}
---
+++ 08.Week8/01.Day1/Services/IContactService.cs
@@ -0,0 +1,13 @@
+﻿using WebApplication7.Models;
+
+namespace WebApplication7.Services
+{
+    public interface IContactService
+    {
+        IEnumerable<ContactInfo> GetAllContacts();
+        ContactInfo GetContactById(int id);
+        void AddContact(ContactInfo contactInfo);
+        void UpdateContact(ContactInfo contactInfo);
+        void DeleteContact(int id);
+    }
+}
