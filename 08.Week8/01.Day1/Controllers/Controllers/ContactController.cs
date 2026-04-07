045d725bc8c652c7f5eea7dbc4fb54e6dc96bc4d
VardireddySupriya <rsupriya2916@gmail.com>
2026-04-07 14:49:32 +0530

    Add files via upload

---
+++ 08.Week8/01.Day1/Controllers/Controllers/ContactController.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication7.Models;
+using WebApplication7.Repository;
+using WebApplication7.Services;
+
+namespace WebApplication7.Controllers
+{
+    public class ContactController : Controller
+    {
+        private readonly IContactService _service;
+        public ContactController(IContactService service)
+        {
+            _service = service;
+        }
+        public IActionResult GetAllContacts()
+        {
+            
+            return View(_service.GetAllContacts());
+        }
+        public IActionResult GetContactById(int id)
+        {
+            var contact = _service.GetContactById(id);
+            if (contact == null)
+            {
+                ViewBag.Error = "data not found";
+                return View();
+            }
+            return View(contact);
+
+        }
+        [HttpGet]
+        public IActionResult AddContact()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddContact(ContactInfo contact)
+        {
+            if(ModelState.IsValid)
+            {
+                 _service.AddContact(contact);
+                return RedirectToAction("GetAllContacts");
+
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "not created";
+                return View();
+            }
+        }
+        public IActionResult EditContact(int id)
+        {
+            var edit = _service.GetContactById(id);
+
+            if (edit == null)
+            {
+                return NotFound();
+            }
+
+            return View(edit);
+        }
+
+        [HttpPost]
+        public IActionResult EditContact(ContactInfo contact)
+        {
+            if (ModelState.IsValid)
+            {
+                _service.UpdateContact(contact);
+                return RedirectToAction("GetAllContacts");
+            }
+
+            ViewBag.Error = "not updated";
+            return View(contact);   
+        }
+
+        [HttpGet]
+        public IActionResult DeleteContact(int id)
+        {
+            var prodObj = _service.GetContactById(id);
+            return View(prodObj);
+        }
+
+
+
+        [HttpPost]
+        [ActionName("DeleteContact")]
+        public IActionResult DeleteConfirm(int id)
+        {
+            var prodObj = _service.GetContactById(id);
+
+            if (prodObj != null)
+            {
+                _service.DeleteContact(id);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "Requested product does not exists";
+                return View();
+            }
+        }
+    }
+}
---
+++ 08.Week8/01.Day1/Controllers/Controllers/ProductsController.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication7.Models;
+using WebApplication7.Repository;
+
+namespace WebApplication7.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly IProductRepository _repo;
+
+        // Injecting ApplicationDbContext in controller 
+        public ProductsController(IProductRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_repo.GetAll());
+        }
+
+
+
+        public IActionResult Details(int id)
+        {
+            var prodObj = _repo.GetById(id);
+            return View(prodObj);
+        }
+
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Products product)
+        {
+            if (ModelState.IsValid)
+            {
+                _repo.Add(product);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "Invalid Product details.";
+                return View();
+            }
+        }
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var prodObj = _repo.GetById(id);
+            return View(prodObj);
+        }
+
+
+        [HttpPost]
+        public IActionResult Edit(Products product)
+        {
+
+            if (ModelState.IsValid)
+            {
+                _repo.Update(product);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "Invalid Product details.";
+                return View();
+            }
+        }
+
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var prodObj = _repo.GetById(id);
+            return View(prodObj);
+        }
+
+
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public IActionResult DeleteConfirm(int id)
+        {
+            var prodObj = _repo.GetById(id);
+
+            if (prodObj != null)
+            {
+                _repo.Delete(id);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "Requested product does not exists";
+                return View();
+            }
+        }
+    }
+}
+
+
