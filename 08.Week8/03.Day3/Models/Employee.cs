05023e70d32220a9bd29e6c7f92cb863170bcdee
VardireddySupriya <rsupriya2916@gmail.com>
2026-04-09 21:10:12 +0530

    Add files via upload

---
+++ 08.Week8/03.Day3/Models/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication9.Models
+{
+    public class Employee
+    {
+        public int EmpId {  get; set; }
+        public String EmpName {  get; set; }
+        public decimal Salary {  get; set; }
+        public DateTime dateTime { get; set; }
+    }
+}
